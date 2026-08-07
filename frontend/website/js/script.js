1df536d783bb039eb607af95c2688d8f2ebc95fa
lucas9080 <lucasferreira9080@outlook.com>
2026-08-07 09:10:38 -0300

    chore: estrutura inicial do projeto

---
+++ frontend/website/js/script.js
@@ -0,0 +1,14 @@
+/*import { getIcons, Alarm, Home, Menu } from '@boxicons/js';
+ 
+// Replace all elements with data-bx attribute
+getIcons({
+  icons: { Alarm, Home, Menu }
+});*/
+
+// Criando ação de login
+
+// API do IMDb
+
+//Interatividades
+
+//testes do frontend
